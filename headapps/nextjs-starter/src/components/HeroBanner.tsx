a8469c1fff9c48c2649a4ea36f117a63c74175bc
Asit Pani <Asit_Pani@epam.com>
2025-08-19 03:58:26 +0530

    fixed - hero banner rich text issue

--- headapps/nextjs-starter/src/components/HeroBanner.tsx
+++ headapps/nextjs-starter/src/components/HeroBanner.tsx
@@ -3,6 +3,7 @@ import {
   Field,
   ComponentParams,
   ImageField,
+  RichText,
   Text as JSSText,
   NextImage as JssImage,
 } from '@sitecore-jss/sitecore-jss-nextjs';
@@ -23,11 +24,10 @@ export const Default = (props: HeroBannerProps): JSX.Element => {
       <div className="row align-items-center bg-light p-4 rounded shadow">
         <div className="col-md-6 text-center text-md-start">
           <h1 className="display-5 fw-bold">
-            {' '}
             <JSSText field={props.fields.Title} />
           </h1>
           <p className="lead">
-            <JSSText field={props.fields.Description} />
+            <RichText field={props.fields.Description} />
           </p>
         </div>
         <div className="col-md-6 text-center">
